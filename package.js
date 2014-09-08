1a8a9bc18fdc83010a684acf36d6363e8982a6eb
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 17:36:14 +0200

    Update for Meteor 0.9.x new package system

--- package.js
+++ package.js
@@ -1,16 +1,19 @@
 Package.describe({
-  summary: "MD5 algorithm for CryptoJS, standard secure algorithms"
+	summary: 'MD5 algorithm for CryptoJS, standard secure algorithms',
+	version: '3.1.2-1',
+	git: 'https://github.com/p-j/meteor-crypto-md5'
 });
 
 Package.on_use(function (api) {
-  api.use('crypto-base', ['client', 'server']);
-  api.imply('crypto-base', ['client', 'server']);
+	api.versionsFrom('METEOR@0.9.0');
+	api.use('jparker:meteor-crypto-base@3.1.2-1', ['client', 'server']);
+	api.imply('jparker:meteor-crypto-base', ['client', 'server']);
 
-  api.add_files('md5.js', ['client', 'server']);
+	api.add_files('md5.js', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use(['crypto-base', 'crypto-md5', 'tinytest']);
+	api.use(['jparker:meteor-crypto-base', 'jparker:meteor-crypto-md5', 'tinytest']);
 
-  api.add_files('crypto-md5_tests.js', ['client', 'server']);
+	api.add_files('crypto-md5_tests.js', ['client', 'server']);
 });
